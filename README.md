cbd5b413276f5cf8df81f0847e787cd242c0b9f6
Reverse04 <ludovicarhiman900@gmail.com>
2023-01-05 15:14:51 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Python-password-gen
 
-At least 12 charater password generator including integer, uppercase and lowercase and symbole ("!","@","?","$","&","#")
+At least 12 charater password generator including integer, uppercase and lowercase and symbole ("!","@","?","$","&","#") it will be automaticly copied in your clipboard
 
 
 
--- password.py
+++ password.py
@@ -1,12 +1,11 @@
-from random import randint
+import random
+import pyperclip
+
 
 symb=["!","@","?","$","&","#"]
 
 
-
-
-
-def pass_def_lengh(lenght):
+def pass_def_lengh():
     '''
     This function define the password length 
     '''
@@ -19,12 +18,52 @@ def pass_def_lengh(lenght):
     return length
 
 
+def random_choice():
+    '''
+    Make random choice between Uppercase, Lowercase, Integer and symbols
+    '''
+    ch=random.randint(1,4)
 
-def password_gen(lenght):
+    if ch == 1:
+        randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
+        return randomLowerLetter
+
+    if ch == 2: 
+        randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
+        return randomUpperLetter
+
+    if ch == 3:
+        randomInteger= random.randint(0,9)
+        return str(randomInteger)
+
+    if ch == 4:
+        randomSymb= (random.choice(symb))
+        return randomSymb      
+
+def list_to_str(list):
+    passwd= " ".join(str(x) for x in list)
     
-    lim=pass_def_lengh(lenght)
+    return passwd.replace(" ","")
+
+
+def password_gen():
+    '''
+    Main function make random password 
+    '''
+    password=[]
+
+    lim=pass_def_lengh()
     
-    for i in range(lim):
-        print('hello')
+    
+    for i in range(0,lim):
+        password.insert(0,random_choice())
+    
+    
+    passSTR=list_to_str(password)
+
+    return pyperclip.copy(list_to_str(password))
+
+print(password_gen())
+
 
 
